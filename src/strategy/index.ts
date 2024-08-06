cabe99854c1d15083d76113d858bbbb9f0078208
dohyunwoo-gint <hyunwoo.do@gintlab.com>
2024-08-06 16:30:32 +0900

    strategy pattern 정의 수정

--- src/strategy/index.ts
+++ src/strategy/index.ts
@@ -3,8 +3,8 @@ import GaussSumStrategy from "./sum-strategy/GaussSumStrategy";
 import LoopSumStrategy from "./sum-strategy/LoopSumStrategy";
 
 /**
- * 전략 패턴은 객체의 행위를 바꾸고 싶을 때 사용하며, 
- * 행위를 클래스로 캡슐화하고 동적으로 객체에 행위를 설정할 수 있게 합니다. 
+ * 전략 패턴은 객체의 기능로직을 바꾸고 싶을 때 사용하며, 
+ * 기능로직 클래스로 캡슐화하고 동적으로 객체에 기능로직을 설정할 수 있게 합니다. 
  * 이는 코드의 유연성과 재사용성을 높여주며, 조건문을 줄여 코드의 가독성을 향상시킵니다. 
  */
 const printer = new SumPrinter()
